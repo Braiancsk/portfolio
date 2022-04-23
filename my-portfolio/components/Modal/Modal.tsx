8a05e791e431be28fe674e2ff2d38cc753457ae0
Braian <braianfelipecsk123@hotmail.com>
2022-04-23 11:10:40 -0300

    Update site

--- my-portfolio/components/Modal/Modal.tsx
+++ my-portfolio/components/Modal/Modal.tsx
@@ -23,32 +23,43 @@ function Modal({ visible, closeModal }: modalTypes) {
         variants={variants}
         className="max-w-[800px] w-full bg-white p-5 relative z-50 rounded-lg shadow"
         >
-        <div >
+        <div>
             <div className="flex justify-end">
                 <AiOutlineCloseCircle className="text-2xl cursor-pointer" onClick={closeModal}/>
             </div>
-            <strong className="block text-black/80 font-bold text-2xl lg:text-3xl text-center">Vamos conversar?</strong>
+            <div className="flex items-center md:items-start md:flex-row flex-col gap-5">
+            <div>
+            <strong className="block text-black/80 font-bold text-xl lg:text-2xl text-center">Vamos conversar?</strong>
             <p className="text-base text-black/80 text-center">Me chama no WhatsApp e faça um orçamento sem compromisso</p>
-            <a title="Me chame no WhatsApp" className="flex justify-center my-5 w-max mx-auto" href="https://wa.me/5562994901223/?text=Eu%20Gostaria%20de%20fazer%20um%20orçamento%20">
-            <IoLogoWhatsapp className="text-green-500 text-5xl"/>
+            <a title="Me chame no WhatsApp" className="flex justify-center my-5 w-max mx-auto transition hover:scale-110" href="https://wa.me/5562994901223/?text=Eu%20Gostaria%20de%20fazer%20um%20orçamento%20">
+            <IoLogoWhatsapp className="text-green-500 text-3xl"/>
             </a>
+            </div>
+           
+            <div>
+              <strong className="block text-black/80 font-bold text-xl lg:text-2xl text-center">Plataformas</strong>
+              <p className="text-base text-black/80 text-center">Você também pode me contratar pelas seguintes plataformas</p>
+
+              <ul className="flex justify-center gap-5 mt-5">
+
+              <li className="transition hover:scale-110 dark:hover:fill-primary icons">
+              <a href="https://www.99freelas.com.br/user/Braian-Fellipe">
+                  <Image src="/99freelas.png" width={30} height={30} alt="icone da empresa 99Freelas, com fundo azul e dois noves desenhados nele"/>
+              </a>
+                  </li>
+
+                  <li className="transition hover:scale-110 dark:hover:fill-primary icons">
+                      <a href="https://www.workana.com/freelancer/1e4b03478c2a79fc2ac42698f3aacd87">
+                          <Image src="/workana.png" width={30} height={30} alt="icone da empresa 99Freelas, com fundo azul e dois noves desenhados nele"/>
+                      </a>
+                  </li>
+              </ul>
+            </div>
+
+            </div>
+          
             
-            <strong className="block text-black/80 font-bold text-2xl lg:text-3xl text-center">Ou me contrate nas plataformas</strong>
-
-            <ul className="flex justify-center gap-5 mt-5">
-    
-            <li className="transition hover:scale-110 dark:hover:fill-primary icons">
-            <a href="https://www.99freelas.com.br/user/Braian-Fellipe">
-                <Image src="/99freelas.png" width={30} height={30} alt="icone da empresa 99Freelas, com fundo azul e dois noves desenhados nele"/>
-            </a>
-                </li>
-
-                <li className="transition hover:scale-110 dark:hover:fill-primary icons">
-                    <a href="https://www.workana.com/freelancer/1e4b03478c2a79fc2ac42698f3aacd87">
-                        <Image src="/workana.png" width={30} height={30} alt="icone da empresa 99Freelas, com fundo azul e dois noves desenhados nele"/>
-                    </a>
-                </li>
-            </ul>
+         
         </div>
     </motion.div>
     </ModalBg>
